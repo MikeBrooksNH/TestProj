b2623ba9cab2b1afd89c8d8817c732ea89c6162a
mike.brooks <m.brooks.photo@gmail.com>
2016-06-17 20:14:12 -0400

    adding some comments

--- calcCompoundInterest/build.sh
+++ calcCompoundInterest/build.sh
@@ -1,8 +1,20 @@
+#!/bin/sh
+
+#
+# Compile code
+#
 gcc -c -o unitTests.o unitTests.c -lm
 gcc -c -o functions.o functions.c -lm
 gcc -c -o main.o main.cpp -lm
 
+#
+# link code to make exe
+#
 gcc -o unitTest unitTests.o functions.o
 gcc -o main functions.o main.o
+
+#
+# Run unit test
+#
 
 ./unitTest
